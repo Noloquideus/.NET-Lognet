f4b8920a9515eda9508251a520cacfec00e88d26
Noloquideus <daniilmanukian@gmail.com>
2024-01-13 20:38:41 +0300

    docs(IHandler): Add documentation for IHandler interface
    Added documentation for the IHandler interface, describing its purpose and the Emit method for emitting log messages based on a log entity and log format.

--- lognet/Handlers/IHandler.cs
+++ lognet/Handlers/IHandler.cs
@@ -2,7 +2,14 @@ using lognet.Domain;
 
 namespace lognet.Handlers;
 
+// Represents an interface for log handlers.
 public interface IHandler
 {
-    public void Emit(LogEntity logEntity, string logFormat);
+    /**
+     * Emits a log message based on the provided log entity and log format.
+     *
+     * @param logEntity The log entity containing log information.
+     * @param logFormat The log format string for formatting the log message.
+     */
+    void Emit(LogEntity logEntity, string logFormat);
 }
