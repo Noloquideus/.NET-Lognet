6662ebb2269bb9d93259b5b49c9c826f1bd3186f
Noloquideus <daniilmanukian@gmail.com>
2024-01-13 20:35:38 +0300

    docs(LogLevel): Add documentation for LogLevel enum
    Added documentation to the LogLevel enum, providing information about each log level and their respective numeric values.

--- lognet/Domain/LogLevel.cs
+++ lognet/Domain/LogLevel.cs
@@ -1,10 +1,21 @@
 namespace lognet.Domain;
 
+
+///  Enum representing different log levels.
 public enum LogLevel
 {
+    // Debug log level (numeric value: 100).
     Debug = 100,
+
+    // Information log level (numeric value: 200).
     Info = 200,
+
+    // Warning log level (numeric value: 300).
     Warning = 300,
+
+    // Error log level (numeric value: 400).
     Error = 400,
+
+    // Critical log level (numeric value: 500).
     Critical = 500
 }
