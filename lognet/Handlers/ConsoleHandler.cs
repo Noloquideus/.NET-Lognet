51ed18db776708c3d66e02aa697c481e4d09ac44
Noloquideus <daniilmanukian@gmail.com>
2024-01-13 20:37:19 +0300

    docs(ConsoleHandler): Add documentation for ConsoleHandler class
    Added documentation for the ConsoleHandler class, describing its purpose and the Emit method for emitting log messages to the console.

--- lognet/Handlers/ConsoleHandler.cs
+++ lognet/Handlers/ConsoleHandler.cs
@@ -2,8 +2,15 @@ using lognet.Domain;
 
 namespace lognet.Handlers;
 
+// Represents a console log handler that emits log messages to the console.
 public class ConsoleHandler : IHandler
 {
+    /**
+     * Emits a log message to the console.
+     *
+     * @param logEntity The log entity containing log information.
+     * @param logFormat The log format string for formatting the log message.
+     */
     public void Emit(LogEntity logEntity, string logFormat)
     {
         string formattedMessage = string.Format(logFormat,
