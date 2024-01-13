647dd6ed2888220e6a3f96238601cfe94e3238ed
Noloquideus <daniilmanukian@gmail.com>
2024-01-13 18:00:49 +0300

    feat: (LoggerConfig.cs) updated LoggerConfig to write logs to a file

--- lognet/Configuration/LoggerConfig.cs
+++ lognet/Configuration/LoggerConfig.cs
@@ -1,20 +1,21 @@
 using lognet.Domain;
 using lognet.Handlers;
 
-namespace lognet.Configuration;
-
-public class LoggerConfig
+namespace lognet.Configuration
 {
-
-    public LoggerConfig(string logFormat, LogLevel minimumLevel, IHandler? consoleHandler = null)
+    public class LoggerConfig
     {
+        public LoggerConfig(string logFormat, LogLevel minimumLevel, IHandler? consoleHandler = null, IHandler? fileHandler = null)
+        {
+            LogFormat = logFormat;
+            MinimumLevel = minimumLevel;
+            ConsoleHandler = consoleHandler;
+            FileHandler = fileHandler;
+        }
 
-        LogFormat = logFormat;
-        MinimumLevel = minimumLevel;
-        ConsoleHandler = consoleHandler;
+        public LogLevel MinimumLevel { get; private set; }
+        public IHandler? ConsoleHandler { get; set; }
+        public IHandler? FileHandler { get; set; }
+        public string LogFormat { get; private set; }
     }
-
-    public LogLevel MinimumLevel { get; private set; }
-    public IHandler? ConsoleHandler { get;  set; }
-    public string LogFormat { get; private set; }
 }
