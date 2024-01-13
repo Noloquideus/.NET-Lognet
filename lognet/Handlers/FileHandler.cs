720171a10fcd0947d22187741c4f5f40b3ca092f
Noloquideus <daniilmanukian@gmail.com>
2024-01-13 20:38:00 +0300

    docs(FileHandler): Add documentation for FileHandler class
    Added documentation for the FileHandler class, describing its purpose, constructor, and the Emit method for emitting log messages to a specified file.

--- lognet/Handlers/FileHandler.cs
+++ lognet/Handlers/FileHandler.cs
@@ -1,23 +1,35 @@
-using System.IO;
 using lognet.Domain;
 
 namespace lognet.Handlers
-{
+{ 
+    //Represents a file log handler that emits log messages to a specified file.
     public class FileHandler : IHandler
     {
         private readonly string _filePath;
 
+        /**
+         * Initializes a new instance of the FileHandler class with the specified file path.
+         *
+         * @param filePath The path to the file where log messages will be written.
+         */
         public FileHandler(string filePath)
         {
             _filePath = filePath;
         }
 
+        /**
+         * Emits a log message to the specified file.
+         *
+         * @param logEntity The log entity containing log information.
+         * @param logFormat The log format string for formatting the log message.
+         */
         public void Emit(LogEntity logEntity, string logFormat)
         {
             using (StreamWriter writer = File.AppendText(_filePath))
             {
-                writer.WriteLine(string.Format(logFormat, logEntity.Level, logEntity.Message));
+                writer.WriteLine(logFormat, logEntity.Level, logEntity.Message);
             }
         }
     }
+
 }
